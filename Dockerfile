5b730dc31532a81876039ac61ce0e41e632fd153
Muhammad Usman <usman.qau77@gmail.com>
2020-09-20 16:57:46 -0400

    Completed-15. Add upload image endpoint

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,10 @@ COPY ./requirements.txt /requirements.txt
 RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories && \
     apk add --update --no-cache postgresql-client
 
+RUN apk add --update --no-cache jpeg-dev
+
 RUN apk add --update --no-cache --virtual .tmp-build-deps \
-    gcc libc-dev linux-headers postgresql-dev
+    gcc libc-dev linux-headers postgresql-dev musl-dev zlib zlib-dev
 
 RUN pip3 install -r /requirements.txt
 
@@ -21,5 +23,9 @@ RUN mkdir /app
 WORKDIR /app
 COPY ./app /app
 
+RUN mkdir -p /vol/web/media
+RUN mkdir -p /vol/web/static
 RUN adduser -D appuser
+RUN chown -R appuser:appuser /vol/
+RUN chmod -R 755 /vol/web
 USER appuser
--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -1,3 +1,7 @@
+import tempfile
+import os
+from PIL import Image
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
@@ -10,6 +14,10 @@ from core.models import Recipe, Tag, Ingredient
 from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 RECIPES_URL = reverse('recipe:recipe-list')
+
+def image_upload_url(recipe_id):
+    """Return URL for recipe image uplaod"""
+    return reverse('recipe:recipe-upload-image', args=[recipe_id])
 
 def detail_url(recipe_id):
     """Return recipe detail URL"""
@@ -192,33 +200,37 @@ class PrivateRecipeApiTests(TestCase):
 
         tags = recipe.tags.all()
         self.assertEqual(len(tags), 0)
+
+class RecipeImageUploadTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email='usman@gmail.com',
+            password='123456'
+        )
+        self.client.force_authenticate(self.user)
+        self.recipe = sample_recipe(user=self.user)
+
+    def tearDown(self):
+        self.recipe.image.delete()
+    
+    def test_upload_image_to_recipe(self):
+        """Test uploading an image to recipe"""
+        url = image_upload_url(self.recipe.id)
+        with tempfile.NamedTemporaryFile(suffix='.jpg') as ntf:
+            img = Image.new('RGB', (10, 10))
+            img.save(ntf, format='JPEG')
+            ntf.seek(0)
+            result = self.client.post(url, {'image': ntf}, format='multipart')
         
+        self.recipe.refresh_from_db()
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertIn('image', result.data)
+        self.assertTrue(os.path.exists(self.recipe.image.path))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-
-
-
-
-
-
-
-
+    def test_upload_image_bad_request(self):
+        """Test uploading an invalid image"""
+        url = image_upload_url(self.recipe.id)
+        result = self.client.post(url, {'image': 'noimage'}, format='multipart')  
+        self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)      
